7b3420dbb1e0276ab9fdc4ae4b6f871df4716c26
Tamreen Khan <historium@gmail.com>
2014-06-13 15:20:04 -0400

    Recompile pharen_debug.phn every time flyfish is run.

--- flyfish.php
+++ flyfish.php
@@ -6,17 +6,22 @@ use Pharen\Lexical as Lexical;
 use \Seq as Seq;
 use \FastSeq as FastSeq;
 Lexical::$scopes['_flyfish'] = array();
-$__scope_id = Lexical::init_closure("_flyfish", 249);
+$__scope_id = Lexical::init_closure("_flyfish", 250);
 include_once 'pharen/path.php';
 use pharen\path as path;
 $flydir = dirname(__FILE__);
-Lexical::bind_lexing("_flyfish", 249, '$flydir', $flydir);
+Lexical::bind_lexing("_flyfish", 250, '$flydir', $flydir);
 function copy_debug_file($cwd){
-	$flydir = Lexical::get_lexical_binding('_flyfish', 249, '$flydir', isset($__closure_id)?$__closure_id:0);;
+	$flydir = Lexical::get_lexical_binding('_flyfish', 250, '$flydir', isset($__closure_id)?$__closure_id:0);;
+	compile_file(($flydir . "/pharen_debug.phn"));
 	return copy(($flydir . "/pharen_debug.php"), ($cwd . "/pharen_debug.php"));
 }
 
 function compile_file($file){
+	return exec(("pharen " . $file));
+}
+
+function compile_debug($file){
 	exec(("pharen -d " . $file));
 	return str_replace(".phn", ".php", $file);
 }
@@ -41,5 +46,5 @@ else{
 
 
 copy_debug_file($cwd);
-$phpfile = compile_file(path\join($cwd, $file));
+$phpfile = compile_debug(path\join($cwd, $file));
 include_once($phpfile);
